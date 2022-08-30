ea11c1732e741f83f4fd04e775155e338b6722b7
Thibaut SIGUENZA <tsiguenz@e2r5p1.clusters.42paris.fr>
2022-08-30 18:45:00 +0200

    alloc with rebind is ok

--- src/AVLTree.hpp
+++ src/AVLTree.hpp
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 17:38:11 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/08/30 16:59:57 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/08/30 18:44:48 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,160 +28,16 @@ namespace ft {
 				this->data = data;
 				left = right = parent = NULL;
 			}
+
+			~Node() { }
 		};
 
 	template<typename T, class Alloc = std::allocator<T> >
 		class AVLTree {
-			public:
+			private:
 
 				typedef Node<T>	node;
 				typedef typename Alloc::template rebind<node>::other AllocNode;
-
-			private:
-
-				Node<T>*	_root;
-				Alloc		_allocPair;
-				Alloc		_allocNode;
-
-				node*	_createNode(T const& val) {
-//					node*	newNode = _allocNode.allocate(1);
-//
-//					_allocNode.construct(&newNode->data, val);
-					node*	newNode = new node(val);
-					return newNode;
-				}
-
-				void	_deallocateNode(node* n) {
-					delete n;
-//					_allocNode.destroy(&n->data);
-//					_allocNode.deallocate(n, 1);
-				}
-
-				int	_getHeight(node* n) {
-					if (n == NULL)
-						return -1;
-					else {
-						int	lheight = _getHeight(n->left);
-						int	rheight = _getHeight(n->right);
-						return ((lheight > rheight) ? lheight : rheight) + 1;
-					}
-				}
-
-				int	_getBalanceFactor(node* n) {
-					if (n == NULL)
-						return -1;
-					return _getHeight(n->left) - _getHeight(n->right);
-				}
-
-				node*	_rightRotate(node* y) {
-					node*	x = y->left;
-					node*	T2 = x->right;
-
-					x->right = y;
-					y->left = T2;
-					return x;
-				}
-
-				node*	_leftRotate(node* x) {
-					node*	y = x->right;
-					node*	T2 = y->left;
-
-					y->left = x;
-					x->right = T2;
-					return y;
-				}
-
-				node*	_insertHelper(node* _root, node* n) {
-					if (_root == NULL) {
-						return n;
-					}
-					if (n->data < _root->data) {
-						_root->left = _insertHelper(_root->left, n);
-						_root->left->parent = _root;
-					}
-					else if (n->data > _root->data) {
-						_root->right = _insertHelper(_root->right, n);
-						_root->right->parent = _root;
-					}
-					else
-						return n;
-					int	bf = _getBalanceFactor(_root);
-					// Left left case
-					if (bf > 1 && n->data < _root->left->data)
-						return _rightRotate(_root);
-					// Right right case
-					if (bf < -1 && n->data > _root->right->data)
-						return _leftRotate(_root);
-					// Left right case
-					if (bf > 1 && n->data > _root->left->data) {
-						_root->left = _leftRotate(_root->left);
-						return _rightRotate(_root);
-					}
-					// Right left case
-					if (bf < -1 && n->data < _root->right->data) {
-						_root->right = _rightRotate(_root->right);
-						return _leftRotate(_root);
-					}
-					return _root;
-				}
-
-				node*	_removeHelper(node* root, T const& key) {
-
-					if (root == NULL)
-						return root;
-					if ( key < root->data )
-						root->left = _removeHelper(root->left, key);
-					else if( key > root->data )
-						root->right = _removeHelper(root->right, key);
-					else {
-						// Node with only one child or no child
-						if( (root->left == NULL) || (root->right == NULL) ) {
-							node* temp = root->left ?
-								root->left :
-								root->right;
-							// No child case
-							if (temp == NULL) {
-								temp = root;
-								root = NULL;
-							}
-							else // One child case
-								*root = *temp;
-							_deallocateNode(temp);
-						}
-						else {
-							// node with two children: Get the inorder
-							// successor (smallest in the right subtree)
-							node* temp = minimum(root->right);
-//							root->data = temp->data;
-							// Destroy and construct for const value
-							_allocPair.destroy(&root->data);
-							_allocPair.construct(&root->data, temp->data);
-							root->right = _removeHelper(root->right, temp->data);
-						}
-					}
-					// If the tree had only one node
-					if (root == NULL)
-						return root;
-
-					int	bf = _getBalanceFactor(root);
-					// Left Left Case
-					if (bf > 1 && _getBalanceFactor(root->left) >= 0)
-						return _rightRotate(root);
-					// Left Right Case
-					if (bf > 1 && _getBalanceFactor(root->left) < 0) {
-						root->left = _leftRotate(root->left);
-						return _rightRotate(root);
-					}
-					// Right Right Case
-					if (bf < -1 && _getBalanceFactor(root->right) <= 0)
-						return _leftRotate(root);
-					// Right Left Case
-					if (bf < -1 && _getBalanceFactor(root->right) > 0) {
-						root->right = _rightRotate(root->right);
-						return _leftRotate(root);
-					}
-					return root;
-				}
 
 			public:
 
@@ -195,7 +51,8 @@ namespace ft {
 						remove(_root->data);
 					}
 				}
-				// TODO implement canonical form
+
+				// TODO implement copy constructor and operator=
 
 				node*	getRoot()
 				{ return _root; }
@@ -246,6 +103,148 @@ namespace ft {
 					inorder(_root->left);
 					std::cout << _root->data << "  ";
 					inorder(_root->right);
+				}
+
+			private:
+
+				Node<T>*	_root;
+				Alloc		_allocPair;
+				AllocNode	_allocNode;
+
+				node*	_createNode(T const& val) {
+					node*	newNode = _allocNode.allocate(1);
+
+					_allocNode.construct(newNode, val);
+					return newNode;
+				}
+
+				void	_deallocateNode(node* n) {
+					_allocNode.destroy(n);
+					_allocNode.deallocate(n, 1);
+				}
+
+				int	_getHeight(node* n) {
+					if (n == NULL)
+						return -1;
+					else {
+						int	lheight = _getHeight(n->left);
+						int	rheight = _getHeight(n->right);
+						return ((lheight > rheight) ? lheight : rheight) + 1;
+					}
+				}
+
+				int	_getBalanceFactor(node* n) {
+					if (n == NULL)
+						return -1;
+					return _getHeight(n->left) - _getHeight(n->right);
+				}
+
+				node*	_rightRotate(node* y) {
+					node*	x = y->left;
+					node*	T2 = x->right;
+
+					x->right = y;
+					y->left = T2;
+					return x;
+				}
+
+				node*	_leftRotate(node* x) {
+					node*	y = x->right;
+					node*	T2 = y->left;
+
+					y->left = x;
+					x->right = T2;
+					return y;
+				}
+
+				node*	_insertHelper(node* _root, node* n) {
+					if (_root == NULL)
+						return n;
+					// Left subtree case
+					if (n->data < _root->data) {
+						_root->left = _insertHelper(_root->left, n);
+						_root->left->parent = _root;
+					}
+					// Right subtree case
+					else if (n->data > _root->data) {
+						_root->right = _insertHelper(_root->right, n);
+						_root->right->parent = _root;
+					}
+					int	bf = _getBalanceFactor(_root);
+					// Left left case
+					if (bf > 1 && n->data < _root->left->data)
+						return _rightRotate(_root);
+					// Right right case
+					if (bf < -1 && n->data > _root->right->data)
+						return _leftRotate(_root);
+					// Left right case
+					if (bf > 1 && n->data > _root->left->data) {
+						_root->left = _leftRotate(_root->left);
+						return _rightRotate(_root);
+					}
+					// Right left case
+					if (bf < -1 && n->data < _root->right->data) {
+						_root->right = _rightRotate(_root->right);
+						return _leftRotate(_root);
+					}
+					return _root;
+				}
+
+				node*	_removeHelper(node* root, T const& key) {
+
+					if (root == NULL)
+						return root;
+					if ( key < root->data )
+						root->left = _removeHelper(root->left, key);
+					else if( key > root->data )
+						root->right = _removeHelper(root->right, key);
+					else {
+						// Node with only one child or no child
+						if( (root->left == NULL) || (root->right == NULL) ) {
+							node* temp = root->left ?
+								root->left :
+								root->right;
+							// No child case
+							if (temp == NULL) {
+								temp = root;
+								root = NULL;
+							}
+							else // One child case
+								*root = *temp;
+							_deallocateNode(temp);
+						}
+						else {
+							// node with two children: Get the inorder
+							// successor (smallest in the right subtree)
+							node* temp = minimum(root->right);
+							// Destroy and construct for const value
+							_allocPair.destroy(&root->data);
+							_allocPair.construct(&root->data, temp->data);
+							root->right = _removeHelper(root->right, temp->data);
+						}
+					}
+					// If the tree had only one node
+					if (root == NULL)
+						return root;
+
+					int	bf = _getBalanceFactor(root);
+					// Left Left Case
+					if (bf > 1 && _getBalanceFactor(root->left) >= 0)
+						return _rightRotate(root);
+					// Left Right Case
+					if (bf > 1 && _getBalanceFactor(root->left) < 0) {
+						root->left = _leftRotate(root->left);
+						return _rightRotate(root);
+					}
+					// Right Right Case
+					if (bf < -1 && _getBalanceFactor(root->right) <= 0)
+						return _leftRotate(root);
+					// Right Left Case
+					if (bf < -1 && _getBalanceFactor(root->right) > 0) {
+						root->right = _rightRotate(root->right);
+						return _leftRotate(root);
+					}
+					return root;
 				}
 		};
 }
